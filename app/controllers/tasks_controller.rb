189c0ff2ac87d86d95cb29701112c0a2ef371490
Kymaeda <kyo.m.720@gmail.com>
2019-06-08 14:20:23 +0900

    show error messages when occuring validation error to create/update tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
 
-    if @task.save!
+    if @task.save
       redirect_to root_path, notice: "タスク【#{@task.name}】を登録しました。"
     else
       render :new
