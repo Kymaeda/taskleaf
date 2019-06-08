8995b527595f1e2e5f46646312a36d46d5262f05
Kymaeda <kyo.m.720@gmail.com>
2019-06-08 12:05:28 +0900

    creates show actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
   def index
     @tasks = Task.all
   end
@@ -24,6 +26,10 @@ class TasksController < ApplicationController
   end
 
   private
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
 
     def task_params
       params.require(:task).permit(:name, :description)
