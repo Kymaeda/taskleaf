6d3d381d9918bcc15be6194f326d529378de6475
Kymaeda <kyo.m.720@gmail.com>
2019-06-08 11:22:00 +0900

    creates new action

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryBot.define do
   factory :task do
     name { "MyString" }
