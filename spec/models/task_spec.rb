20c78215d8be50bd9bc61b9c7e7503f3e708dce2
Kymaeda <kyo.m.720@gmail.com>
2019-06-08 22:22:23 +0900

    adds references to tasks and association to taskModel and userModel

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,6 +7,11 @@
 #  name        :string(30)       not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_tasks_on_user_id  (user_id)
 #
 
 require 'rails_helper'
