7564d3717cea112aa097289ca799d8743db0d445
Kymaeda <kyo.m.720@gmail.com>
2019-06-08 22:34:19 +0900

    change controller to show tasks only the login user has

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.task.new(task_params)
 
     if @task.save
       redirect_to root_path, notice: "タスク【#{@task.name}】を登録しました。"
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   private
 
     def set_task
-      @task = Task.find(params[:id])
+      @task = current_user.tasks.find(params[:id])
     end
 
     def task_params
