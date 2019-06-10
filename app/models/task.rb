57d3a432d3b9c6ddebb848bb2a1f65827aa3f7eb
Kymaeda <kyo.m.720@gmail.com>
2019-06-11 08:46:06 +0900

    restricts columns and associations you could search by rasack

--- app/models/task.rb
+++ app/models/task.rb
@@ -29,4 +29,12 @@ class Task < ApplicationRecord
     pending:   3,
     completed: 4,
   }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
