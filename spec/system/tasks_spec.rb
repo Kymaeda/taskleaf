8b3bff0a7e1b2ff8ded157358773ecc1701119b6
Kymaeda <kyo.m.720@gmail.com>
2019-06-16 12:29:50 +0900

    unify redundant expression

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,25 +2,26 @@ require 'rails_helper'
 
 describe "タスク管理機能", type: :system do
   describe "一覧表示機能" do
+    # ユーザを作成
+    let(:user_a){ FactoryBot.create(:user, name: "ユーザA", email: "a@example.com") }
+    let(:user_b){ FactoryBot.create(:user, name: "ユーザB", email: "b@example.com") }
+
     before do
-      # ユーザAを作成
-      user_a = FactoryBot.create(:user, name: "ユーザA", email: "a@example.com")
       # 作成者がユーザAであるタスクを作成
       FactoryBot.create(:task, name: "最初のタスク", user: user_a)
+      # ユーザでログインする
+      ## ログイン画面にアクセスする
+      visit(login_path)
+      ## メールアドレスを入力する
+      fill_in "メールアドレス",	with: login_user.email
+      ## パスワードを入力する
+      fill_in "パスワード",	with: login_user.password
+      ## ログインボタンを押す
+      click_button 'ログインする'
     end
 
     context "ユーザAがログインしている時" do
-      before do
-        # ユーザAでログインする
-        ## ログイン画面にアクセスする
-        visit(login_path)
-        ## メールアドレスを入力する
-        fill_in "メールアドレス",	with: "a@example.com"
-        ## パスワードを入力する
-        fill_in "パスワード",	with: "password"
-        ## ログインボタンを押す
-        click_button 'ログインする'
-      end
+      let(:login_user){ user_a }
 
       it "ユーザAが作成したタスクが表示される" do
         # 作成済みのタスクが一覧画面上に表示されていることを確認する
@@ -29,19 +30,7 @@ describe "タスク管理機能", type: :system do
     end
 
     context "ユーザBがログインしている時" do
-      before do
-        # ユーザBを作成する
-        user_b = FactoryBot.create(:user, name: "ユーザB", email: "b@example.com")
-        # ユーザBでログインする
-        ## ログイン画面にアクセスする
-        visit(login_path)
-        ## メールアドレスを入力する
-        fill_in "メールアドレス",	with: "b@example.com"
-        ## パスワードを入力する
-        fill_in "パスワード",	with: "password"
-        ## ログインボタンを押す
-        click_button 'ログインする'
-      end
+      let(:login_user){ user_b }
 
       it "ユーザAが作成したタスクが表示されない" do
         # ユーザAが作成したタスクが、一覧画面上に表示されていないことを確認する
