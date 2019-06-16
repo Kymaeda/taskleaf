f938e4d5274b2702957e839af110e491772d8614
Kymaeda <kyo.m.720@gmail.com>
2019-06-16 12:43:57 +0900

    appends spec for show action and revise codes to be DRY

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,30 +1,22 @@
 require 'rails_helper'
 
 describe "タスク管理機能", type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: "ユーザA", email: "a@example.com") }
+  let(:user_b) { FactoryBot.create(:user, name: "ユーザB", email: "b@example.com") }
+  let!(:task_a){ FactoryBot.create(:task, name: "最初のタスク", user: user_a) }
+
+  before do
+    visit login_path
+    fill_in "メールアドレス",	with: login_user.email
+    fill_in "パスワード",	with: login_user.password
+    click_button 'ログインする'
+  end
+
   describe "一覧表示機能" do
-    # ユーザを作成
-    let(:user_a){ FactoryBot.create(:user, name: "ユーザA", email: "a@example.com") }
-    let(:user_b){ FactoryBot.create(:user, name: "ユーザB", email: "b@example.com") }
-
-    before do
-      # 作成者がユーザAであるタスクを作成
-      FactoryBot.create(:task, name: "最初のタスク", user: user_a)
-      # ユーザでログインする
-      ## ログイン画面にアクセスする
-      visit(login_path)
-      ## メールアドレスを入力する
-      fill_in "メールアドレス",	with: login_user.email
-      ## パスワードを入力する
-      fill_in "パスワード",	with: login_user.password
-      ## ログインボタンを押す
-      click_button 'ログインする'
-    end
-
     context "ユーザAがログインしている時" do
       let(:login_user){ user_a }
 
       it "ユーザAが作成したタスクが表示される" do
-        # 作成済みのタスクが一覧画面上に表示されていることを確認する
         expect(page).to have_content "最初のタスク"
       end
     end
@@ -33,8 +25,21 @@ describe "タスク管理機能", type: :system do
       let(:login_user){ user_b }
 
       it "ユーザAが作成したタスクが表示されない" do
-        # ユーザAが作成したタスクが、一覧画面上に表示されていないことを確認する
         expect(page).to have_no_content "最初のタスク"
+      end
+    end
+  end
+
+  describe "詳細表示機能" do
+    context "ユーザAがログインしている時" do
+      let(:login_user){ user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it "ユーザAが作成したタスクが表示される" do
+        expect(page).to have_content "最初のタスク"
       end
     end
   end
